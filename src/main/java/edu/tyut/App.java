51cf916843f823deada1fbac8acafc9870012f16
zhangshuhao <2905154639@qq.com>
2022-07-01 21:46:29 +0800

    先初始化一下仓库

---
+++ src/main/java/edu/tyut/App.java
@@ -0,0 +1,14 @@
+package edu.tyut;
+
+/**
+ * Hello world!
+ * 可以完成很多游戏
+ * 五子棋、连连看、俄罗斯方块、扫雷
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/edu/tyut/game/GobangSimple.java
@@ -0,0 +1,5 @@
+package edu.tyut.game;
+
+public class GobangSimple {
+    // 定义棋盘大小
+}
